e908df89598a94ee57df96e33932f8b0c10896a6
PRAVEENKUMAR <121844437+CoderBot01@users.noreply.github.com>
2023-05-06 19:22:39 +0530

    Create if.py

---
+++ if.py
@@ -0,0 +1,13 @@
+x=int(input("Enter an integer : "))
+if x < 0:
+    x=0
+    print('Negative changed to zero')
+elif x==0:
+    print('zero')
+elif x==1:
+    print('single')
+else:
+    print('more')
+
+    # the if, elif and else helps to find the correct value what we want like this or that
+    #it is like a switch or case statements
