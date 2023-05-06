0c3eb5f85019bcff49a04e462013ac8a0712470f
PRAVEENKUMAR <121844437+CoderBot01@users.noreply.github.com>
2023-05-06 19:14:40 +0530

    Create fibonacciseries.py

---
+++ fibonacciseries.py
@@ -0,0 +1,9 @@
+a,b=0,1
+while a < 10:
+     print(a)
+     a,b = b, a+b
+#fibonacci series is like the sum of two elements defines the next value
+a,b=0,1
+while a<900:
+     print(a, end=",")
+     a,b=b,a+b
